f3097025cfbed829be5dc2a2ed6958fc0b04e681
Ioannis Mertzanis <imertz@protonmail.com>
2022-12-14 20:05:40 +0200

    Removed dark elements

--- src/App.tsx
+++ src/App.tsx
@@ -126,7 +126,7 @@ export default function App() {
       </h1>
 
       <input
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
         type="text"
         id="message"
         name="message"
@@ -137,7 +137,7 @@ export default function App() {
       <div className="max-w-xs pt-2">
         <select
           value={selected}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
           onChange={handleSelect}
         >
           {options.map((option) => (
